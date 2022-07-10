e91bf8a714788ccddc7624a268adc82d49309bcf
Sala2Code <109032171+Sala2Code@users.noreply.github.com>
2022-07-10 18:00:20 +0200

    Add files via upload

--- serf.hpp
+++ serf.hpp
@@ -1,59 +1,41 @@
-#pragma once
-#include <iostream>
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
+// ! V2 : Horizontal moving 
 
-#include <cmath>
-#include <ctime>
-#include <cstdlib>
-#include <math.h>       
-#define PI 3.14159265
+////////////////////////////////////////////////////////////
+//You can modify this :
+
+const int width = 1700; //Window
+const int height = 1050; //Window
+const int taille = 40; //Boid
+int amplitude = 20; //Not really useful
+const int zone_prox = 8*taille; 
+const int zone_align = 4*taille;
+const int zone_collision = taille*1.5; 
+float vitesse = 0.5; //Speed
+const int nb_total = 50; //Number of boids after generation
+const int speed_generate = 30; //Accelerate the generation
+const int nb_to_generate = 3; //For each generation how can be generated.
 
 
 ////////////////////////////////////////////////////////////
+//For the V1, let like that
+const int marge = 50; //Distance to the window's bordure 
+const int config[2] = {2,2}; //row , col
 
+////////////////////////////////////////////////////////////
 sf::ContextSettings options;
-int width = 1700;
-int height = 1050;
-int fps = 30;
-
-int taille = 20;
+const int fps = 30;
+const float cote = taille*3 / (2*sqrt(3));
 int nb_step = 0;
+int nb_serf = config[0]*config[1];
+sf::VertexArray list_serf[nb_total];
+
 int degre;
-float cote = taille*3 / (2*sqrt(3));
-int amplitude = 12;
-float vitesse = 0.4;
 bool mur;
 int row;
 int col;
-const int zone_prox = 10*taille;
-const int zone_align = 5*taille;
-const int zone_collision = taille*3;
-const int marge = 50;
-
-const int config[2] = {8,8}; //row , col
-const int nb_serf = config[0]*config[1];
-sf::VertexArray list_serf[nb_serf];
 std::vector<float> long_pas;
 std::vector<float> pas;
 std::vector<float> centre;
-
 sf::RenderWindow window;
-sf::RectangleShape carre;
 
 ////////////////////////////////////////////////////////////
-
-sf::Text text;
-sf::Text t_autre;
-sf::Text console;
-sf::Font font;
-void LoadFont();
-void SetText(sf::Text& txt, sf::String str);
-
-
-
-// class Serf {
-//     public:
-
-// };
-
